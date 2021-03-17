4d3721501b8d6319e628cb2a13b06b2358baf7b8
dreamdreamer123 <ns52686@fer.hr>
2021-03-17 14:05:33 +0100

    Initial commit

--- app/src/main/java/com/example/animals/data/AddFragment.kt
+++ app/src/main/java/com/example/animals/data/AddFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProviders
+import com.example.animals.MainViewModel
 import com.example.animals.R
+import com.example.animals.util.InjectorUtils
+import com.example.animals.viewModel.Animal
 
 class AddFragment : Fragment() {
 
@@ -15,5 +22,6 @@ class AddFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+
     }
 }
